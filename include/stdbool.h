df28a4d3bbb034bd56bac09e207b7fea1a4e6782
Jerpism <jere.m@live.fi>
2024-01-18 16:04:34 +0200

    Add <stdbool.h> and header file for vga

---
+++ include/stdbool.h
@@ -0,0 +1,9 @@
+#ifndef STDBOOL_H_
+#define STDBOOL_H_
+
+typedef _Bool   bool;
+
+#define false   0 
+#define true    1
+
+#endif
---
+++ include/vga.h
@@ -0,0 +1,4 @@
+#ifndef VGA_H_
+#define VGA_H_
+
+#endif /* VGA_H */
