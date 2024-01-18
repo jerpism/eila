557362131899fcaf34747f71569a00cb36571dc4
Jerpism <jere.m@live.fi>
2024-01-18 12:46:03 +0200

    Add XD testprint to confirm kernel is loading

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,7 +1,15 @@
-#define VGA_MEM 0xb800
 
 void main(){
+    unsigned char *vga = (unsigned char*)0xb8000;
 
+    /* Print out a XD at the top of the screen*/
+    *vga = 'X';
+    *(vga+1) = 0x0f;
+
+    vga+=2;
+
+    *vga = 'D';
+    *(vga+1) = 0x0f;
 
     return;
 }
