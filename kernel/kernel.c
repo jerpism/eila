b4ee78d22d8a1a6a682f675a181dd5288915296d
Jerpism <jere.m@live.fi>
2024-01-18 20:00:00 +0200

    Change kernel print to call vga test_print

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,16 +1,10 @@
 #include <stdint.h>
+#include <stdbool.h>
+#include <vga.h>
+#include <io.h>
+
 
 void main(){
-    uint8_t *vga = (unsigned char*)0xb8000;
-
-    /* Print out a XD at the top of the screen*/
-    *vga = 'X';
-    *(vga+1) = 0x0f;
-
-    vga+=2;
-
-    *vga = 'D';
-    *(vga+1) = 0x0f;
-
+    test_print();
     return;
 }
