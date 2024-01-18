a39e3024187e0d5043598cd1b18f546566a5c5b5
Jerpism <jere.m@live.fi>
2024-01-18 20:02:08 +0200

    add <vga.h>

--- include/vga.h
+++ include/vga.h
@@ -1,4 +1,12 @@
 #ifndef VGA_H_
 #define VGA_H_
+#include <stdint.h>
 
-#endif /* VGA_H */
+/* TODO: remove this at some point when no longer necessary */
+void test_print();
+
+void print(char*);
+void print_char(uint8_t character, int color, int col, int row);
+
+void clear_screen();
+#endif /* VGA_H_ */
