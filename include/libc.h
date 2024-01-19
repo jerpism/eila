f53f436b466099d4c4a8abc952f192d6bc8c8b98
Jerpism <jere.m@live.fi>
2024-01-19 10:51:23 +0200

    Add itoa and fix strlen not working for some reason

--- include/libc.h
+++ include/libc.h
@@ -2,5 +2,9 @@
 #define LIBC_H_
 #include <stdint.h>
 
+size_t strlen(const char*);
+size_t strcpy(char*, const char*);
+size_t memcpy(uint8_t*, uint8_t*, size_t);
+char *itoa(int, char*, int);
 
 #endif /* LIBC_H_ */
--- lib/libc.c
+++ lib/libc.c
@@ -12,8 +12,8 @@ size_t strlen(const char *s){
 size_t strcpy(char *dst, const char *src){
     size_t len = 0;
 
-    for(; src[len]; ++len){
-        *dst++ = *src++;
+    for(len = 0; src[len]; ++len){
+        dst[len] = src[len];
     }
 
     dst[len] = '\0';
@@ -30,6 +30,49 @@ size_t memcpy(uint8_t *dst, uint8_t *src, size_t n){
         *dst++ = *src++;
     }
     return i;
+}
+
+
+/* Mostly copied from wiki.osdev.org
+ * but only supports up to base 16 for now */
+char *itoa(int value, char *s, int base){
+    char *rc;
+    char *ptr;
+    char *low;
+
+    /* See if base is supported */
+    if(base < 2 || base > 16){
+        *s = '\0';
+        return s;
+    }
+
+    rc = ptr = s;
+
+    /* Set minus sign if negative decimal */
+    if(value < 0 && base == 10){
+        *ptr++ = '-';
+    }
+
+    /* Start point of the actual numbers */
+    low = ptr;
+
+    do{
+        /* Clever trick to get rid of abs() */
+        *ptr++ = "fedcba9876543210123456789abcdef"[15 + value % base];
+        value /= base;
+    }while(value);
+
+    /* Terminate string */
+    *ptr-- = '\0';
+
+    /* And reverse it so it's the right way around 
+     * Maybe replace this with a clever */
+    while(low < ptr){
+        char tmp = *low;
+        *low++ = *ptr;
+        *ptr-- = tmp;
+    }
+    return rc;
 }
 
 
